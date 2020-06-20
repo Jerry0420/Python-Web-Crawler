ca5ce00c66aa54c4d6fbaeacf93837c6c69f943b
Jerry Wang <jerrywang@aten.com.tw>
2020-06-20 13:28:40 +0800

    ddddddd

--- fragrantica/crawler.py
+++ fragrantica/crawler.py
@@ -29,22 +29,41 @@ class Crawler(BaseCrawler):
     def __init__(self, process_num, session):
         super().__init__(process_num=process_num, session=session)
 
-    def job(self, url):
-        document = self.session.get(url)
-        document = BeautifulSoup(document, 'lxml')
-        contents = document.select('ul.boxify-container li.boxify-item.product-item')
+    def job(self, perfumer, page):
         result = []
-        for content in contents:
-            title = content.select_one('div.title').text.strip()
-            result.append({
-                'title': title
-            })
-        logger.info('Got %s items', len(result))
+        query_strings = (
+            ('x-algolia-application-id', 'FGVI612DFZ'),
+            ('x-algolia-api-key', 'NDI2YWY5NjgyY2FjOWE0MTkyODI2YTI4OGZhNmRkOTE4ZWUxMjBhMTA3NzFkMGE1MDVkZmViOGQwZjcwNmYxOXZhbGlkVW50aWw9MTU5MzQyNjgzNQ=='),
+        )
+
+        body = '{"requests":[{"indexName":"fragrantica_perfumes","params":'
+        params = "\"hitsPerPage=80&maxValuesPerFacet=10&page={page}&attributesToRetrieve=%5B%22naslov%22%2C%22dizajner%22%2C%22url.EN%22%2C%22thumbnail%22%5D&facets=%5B%22spol%22%2C%22dizajner%22%5D&facetFilters=%5B%5B%22nosevi%3A{perfumer}%22%5D%5D\"".format(page=page, perfumer=perfumer)
+        body = ''.join([body, params, '}]}'])
+
+        url = "https://fgvi612dfz-3.algolianet.com/1/indexes/*/queries"
+
+        response = self.session.post(url=url, query_strings=query_strings, body=body, json_response=True)
+        try:
+            print(response['results'][0]['hits'][0])
+        except Exception as error:
+            logger.exception(error)
+        
         return result
     
     def loop_perfumers(self, perfumer):
         perfumer = perfumer
-        
+        page = 0
+        results = []
+        while True:
+            try:
+                items = self.job(perfumer, page)
+                results.extend(items)
+                logger.info('Got %s of %s in page %s', len(items), perfumer, page)
+                page += 1
+            except Exception as error:
+                logger.exception(error)
+                break
+        return results
 
     def collect_perfumers(self):
         url = "https://www.fragrantica.asia/noses/"
@@ -60,9 +79,10 @@ class Crawler(BaseCrawler):
         return list(perfumers)
 
     def start_crawler(self):
-        perfumers = self.collect_perfumers()
+        # perfumers = self.collect_perfumers()
+        perfumers = ['Alberto Morillas']
         try:
-            count = self.map(self.job, perfumers)
+            count = self.map(self.loop_perfumers, perfumers)
             logger.info('Saved %s items', count)
             self.save()
         except Exception as error:
