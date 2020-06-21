dd74b2f36f9f0e56376075d5c4695ab805062ca3
JerryWang <jerry@funmula.com>
2020-06-22 00:09:49 +0800

    qqqqqq

--- fragrantica/crawler_selenium.py
+++ fragrantica/crawler_selenium.py
@@ -76,43 +76,43 @@ class CrawlerSelenium(BaseCrawler):
     
     def start_crawler(self):
 
-        # file_content = {}
-        # with open('all_perfumes_simple.json', 'r') as f:
-        #     file_content = json.loads(f.read())
+        file_content = {}
+        with open('all_perfumes_simple.json', 'r') as f:
+            file_content = json.loads(f.read())
 
-        file_content = {
-            "48688": {
-                "title": "Fabruary 14\u00ba", 
-                "brand": "The Society of Scent", 
-                "product_id": "48688", 
-                "url": "https://www.fragrantica.com/perfume/The-Society-of-Scent/Fabruary-14--48688.html"
-                }, 
-            "60283": {
-                "title": "Asphalt Noir(e)", 
-                "brand": "The Society of Scent", 
-                "product_id": "60283", 
-                "url": "https://www.fragrantica.com/perfume/The-Society-of-Scent/Asphalt-Noir-e--60283.html"
-                },
-            "19991": {
-                "title": "Saison d'Amour", 
-                "brand": "Novaya Zarya \u041d\u043e\u0432\u0430\u044f \u0417\u0430\u0440\u044f", 
-                "product_id": "19991", 
-                "url": "https://www.fragrantica.com/perfume/Novaya-Zarya-/Saison-d-Amour-19991.html"
-                }, 
-            "19987": {
-                "title": "Mon Bijou Vert", 
-                "brand": "Novaya Zarya \u041d\u043e\u0432\u0430\u044f \u0417\u0430\u0440\u044f", 
-                "product_id": "19987", 
-                "url": "https://www.fragrantica.com/perfume/Novaya-Zarya-/Mon-Bijou-Vert-19987.html"
-                }
-        }
+        # file_content = {
+        #     "48688": {
+        #         "title": "Fabruary 14\u00ba", 
+        #         "brand": "The Society of Scent", 
+        #         "product_id": "48688", 
+        #         "url": "https://www.fragrantica.com/perfume/The-Society-of-Scent/Fabruary-14--48688.html"
+        #         }, 
+        #     "60283": {
+        #         "title": "Asphalt Noir(e)", 
+        #         "brand": "The Society of Scent", 
+        #         "product_id": "60283", 
+        #         "url": "https://www.fragrantica.com/perfume/The-Society-of-Scent/Asphalt-Noir-e--60283.html"
+        #         },
+        #     "19991": {
+        #         "title": "Saison d'Amour", 
+        #         "brand": "Novaya Zarya \u041d\u043e\u0432\u0430\u044f \u0417\u0430\u0440\u044f", 
+        #         "product_id": "19991", 
+        #         "url": "https://www.fragrantica.com/perfume/Novaya-Zarya-/Saison-d-Amour-19991.html"
+        #         }, 
+        #     "19987": {
+        #         "title": "Mon Bijou Vert", 
+        #         "brand": "Novaya Zarya \u041d\u043e\u0432\u0430\u044f \u0417\u0430\u0440\u044f", 
+        #         "product_id": "19987", 
+        #         "url": "https://www.fragrantica.com/perfume/Novaya-Zarya-/Mon-Bijou-Vert-19987.html"
+        #         }
+        # }
 
         try:
             for key, value in file_content.items():
                 try:
                     perfume = self.open_browser(key, value)
                     if perfume:
-                        logger.info('Got %s', perfume['url'])
+                        logger.info('Got %s', perfume)
                         self.append([perfume])
                 except Exception as error:
                     logger.error('Fail to get page %s', value['url'])
