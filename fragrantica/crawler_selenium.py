7aaefadb1c1c2b1ab4372b9b23d497a44e753c9b
JerryWang <jerry@funmula.com>
2020-06-23 00:00:30 +0800

    qqqqq

--- fragrantica/crawler_selenium.py
+++ fragrantica/crawler_selenium.py
@@ -3,8 +3,8 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
 
-from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import json
 from bs4 import BeautifulSoup
 import time
@@ -23,16 +23,32 @@ class CrawlerSelenium(BaseCrawler):
 
     def __init__(self):
         super().__init__()
-        self.firefox_profile = webdriver.FirefoxProfile()
-        self.firefox_profile.set_preference('permissions.default.image', 2)
-        self.firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
-        
-        self.opts = Options()
-        self.opts.set_headless(headless=True)
-        self.opts.add_argument('--incognito')
 
-        self.driver = webdriver.Firefox(options=self.opts, firefox_profile=self.firefox_profile)
-        # self.driver = webdriver.Firefox()
+        self.opts = webdriver.ChromeOptions()
+        self.opts.add_argument('--headless')
+        self.opts.add_argument('--incognito')
+        self.opts.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36)')
+        self.opts.add_argument('--disable-dev-shm-usage')
+        self.opts.add_argument('--disable-gpu')
+        self.opts.add_argument('--no-sandbox')
+        self.opts.add_argument('--allow-running-insecure-content')
+        self.opts.add_experimental_option('useAutomationExtension', False)
+        self.opts.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+        self.opts.experimental_options["prefs"] = {
+            "profile.default_content_settings": {"images": 2},
+            "profile.managed_default_content_settings": {"images": 2}
+        }
+
+    def reload_driver(self):
+        # PROXY = ""
+        webdriver.DesiredCapabilities.CHROME['proxy']={
+            "httpProxy":PROXY,
+            "ftpProxy":PROXY,
+            "sslProxy":PROXY,
+            "proxyType":"MANUAL",
+        }
+        self.driver = webdriver.Chrome(options=self.opts, desired_capabilities=DesiredCapabilities.CHROME)
 
     def open_browser(self, product_id, content):
         url = content['url']
@@ -75,37 +91,38 @@ class CrawlerSelenium(BaseCrawler):
         return result
     
     def start_crawler(self):
+        self.reload_driver()
 
-        file_content = {}
-        with open('all_perfumes_simple.json', 'r') as f:
-            file_content = json.loads(f.read())
+        # file_content = {}
+        # with open('all_perfumes_simple.json', 'r') as f:
+            # file_content = json.loads(f.read())
 
-        # file_content = {
-        #     "48688": {
-        #         "title": "Fabruary 14\u00ba", 
-        #         "brand": "The Society of Scent", 
-        #         "product_id": "48688", 
-        #         "url": "https://www.fragrantica.com/perfume/The-Society-of-Scent/Fabruary-14--48688.html"
-        #         }, 
-        #     "60283": {
-        #         "title": "Asphalt Noir(e)", 
-        #         "brand": "The Society of Scent", 
-        #         "product_id": "60283", 
-        #         "url": "https://www.fragrantica.com/perfume/The-Society-of-Scent/Asphalt-Noir-e--60283.html"
-        #         },
-        #     "19991": {
-        #         "title": "Saison d'Amour", 
-        #         "brand": "Novaya Zarya \u041d\u043e\u0432\u0430\u044f \u0417\u0430\u0440\u044f", 
-        #         "product_id": "19991", 
-        #         "url": "https://www.fragrantica.com/perfume/Novaya-Zarya-/Saison-d-Amour-19991.html"
-        #         }, 
-        #     "19987": {
-        #         "title": "Mon Bijou Vert", 
-        #         "brand": "Novaya Zarya \u041d\u043e\u0432\u0430\u044f \u0417\u0430\u0440\u044f", 
-        #         "product_id": "19987", 
-        #         "url": "https://www.fragrantica.com/perfume/Novaya-Zarya-/Mon-Bijou-Vert-19987.html"
-        #         }
-        # }
+        file_content = {
+            "48688": {
+                "title": "Fabruary 14\u00ba", 
+                "brand": "The Society of Scent", 
+                "product_id": "48688", 
+                "url": "https://www.fragrantica.com/perfume/The-Society-of-Scent/Fabruary-14--48688.html"
+                }, 
+            "60283": {
+                "title": "Asphalt Noir(e)", 
+                "brand": "The Society of Scent", 
+                "product_id": "60283", 
+                "url": "https://www.fragrantica.com/perfume/The-Society-of-Scent/Asphalt-Noir-e--60283.html"
+                },
+            "19991": {
+                "title": "Saison d'Amour", 
+                "brand": "Novaya Zarya \u041d\u043e\u0432\u0430\u044f \u0417\u0430\u0440\u044f", 
+                "product_id": "19991", 
+                "url": "https://www.fragrantica.com/perfume/Novaya-Zarya-/Saison-d-Amour-19991.html"
+                }, 
+            "19987": {
+                "title": "Mon Bijou Vert", 
+                "brand": "Novaya Zarya \u041d\u043e\u0432\u0430\u044f \u0417\u0430\u0440\u044f", 
+                "product_id": "19987", 
+                "url": "https://www.fragrantica.com/perfume/Novaya-Zarya-/Mon-Bijou-Vert-19987.html"
+                }
+        }
 
         try:
             for key, value in file_content.items():
@@ -117,7 +134,8 @@ class CrawlerSelenium(BaseCrawler):
                 except Exception as error:
                     logger.error('Fail to get page %s', value['url'])
                     self.retry_info.append({key: value})
-                time.sleep(5)
+                    self.reload_driver()
+                time.sleep(15)
         except KeyboardInterrupt as keyboard_error:
             logger.exception(keyboard_error)
         except Exception as error:
